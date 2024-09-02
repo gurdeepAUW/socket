6558075721446fe036904ea8b3cde791d2359b7a
gurdeepAUW <gurdeep.singh@auw.co.in>
2024-09-01 23:33:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Handle socket connections
 io.on('connection', (socket) => {
