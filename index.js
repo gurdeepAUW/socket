0600b336acdeb896eb6755b052aa4aa722dd60dc
gurdeepAUW <gurdeep.singh@auw.co.in>
2024-09-02 03:25:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const {Server} = require('socket.io');
 const cors = require('cors');
-
+ 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*', // Allow all origins (use a specific origin if needed)
-    methods: ['GET', 'POST']
-  }
+    methods: ['GET', 'POST'],
+  },
 });
-
+ 
 // Use CORS middleware
 app.use(cors());
-
+ 
 // Log when a user connects
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log(`A user connected: ${socket.id}`);
-
+ 
+  // Listen for 'sendMessage' events from clients
+  socket.on('sendMessage', message => {
+    console.log('Received message:', message);
+    // Emit a response back to the client
+    socket.emit('message', 'Message received: Rahul sasajs' + message);
+  });
+ 
+  // const interval = setInterval(() => {
+  //   io.emit('message', 'Hello from the server!');
+  //   console.log("message sent to client ");
+  // }, 2000); // 10 seconds
+ 
   socket.on('disconnect', () => {
     console.log(`A user disconnected: ${socket.id}`);
-  });
+    //clearInterval(interval);
+     });
 });
-
+ 
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
